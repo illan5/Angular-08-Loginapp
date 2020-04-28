ea78ffd170a2345d8d98b079605fc3ac41bc5527
Jose Illan <jose_illan@hotmail.com>
2020-04-28 23:57:46 +0200

    Sign up function created

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -25,7 +25,22 @@ export class AuthService {
 
   }
   nuevoUsuario( usuario: UsuarioModel ) {
-    
+    // const authData = {
+    //   email: usuario.email,
+    //   password: usuario.password,
+    //   returnSecureToken: true
+    // };
+
+    const authData = {
+      ...usuario,
+      returnSecureToken: true
+    };
+
+    return this.http.post(
+      `${ this.url }signUp?key=${ this.apikey }`,
+      authData
+    );
+
   }
 
 }
