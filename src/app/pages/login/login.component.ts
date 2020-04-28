1fd0eee3abe282605d3a622b8889246d52c3109a
Jose Illan <jose_illan@hotmail.com>
2020-04-29 00:47:37 +0200

    Add SweetAlert2 effects and route to /home

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,6 +3,9 @@ import { NgForm } from '@angular/forms';
 import { UsuarioModel } from 'src/app/models/usuario.model';
 import { AuthService } from 'src/app/services/auth.service';
 
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,18 +15,33 @@ export class LoginComponent implements OnInit {
 
   usuario: UsuarioModel = new UsuarioModel()
 
-  constructor( private auth: AuthService ) { }
+  constructor( private auth: AuthService,
+               private router: Router ) { }
 
   ngOnInit() {
   }
 
   login( form: NgForm ){
     if ( form.invalid ) { return; }
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Espere por favor...'
+    });
+    Swal.showLoading();
+
     this.auth.login( this.usuario )
         .subscribe( resp => {
           console.log(resp);
+          Swal.close();
+          this.router.navigateByUrl('/home');
         }, (err) => {
           console.log(err.error.error.message);
+          Swal.fire({
+            icon: 'error',
+            title: 'Error al autenticar',
+            text: err.error.error.message
+          });
         });
 
     console.log(this.usuario);
