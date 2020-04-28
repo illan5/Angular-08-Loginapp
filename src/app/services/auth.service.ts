1fb8535c5831d5286eae8f992c5dbbb38b8ee066
Jose Illan <jose_illan@hotmail.com>
2020-04-28 23:41:12 +0200

    Create auth service and define methods

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UsuarioModel } from '../models/usuario.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private url = 'https://identitytoolkit.googleapis.com/v1/accounts:';
+  private apikey = 'AIzaSyDRX2jAs11T3wSEJupUWLvZGzVZBKKf8Zc'
+;
+  // Sign up new user
+  // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
+
+  // Login
+  // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
+
+  constructor( private http: HttpClient ) { }
+  
+  logout() {
+
+  }
+  login( usuario: UsuarioModel ) {
+
+  }
+  nuevoUsuario( usuario: UsuarioModel ) {
+    
+  }
+
+}
